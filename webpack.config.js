183536bd533bd10f53257e633d13c15e630d1a13
ahpx <ahpx@yandex.com>
2024-09-20 22:41:17 +0800

    Fixed webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true
   },
   plugins: [
-    new HtmlWebpackPlugin(),
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: 'src/template.html'
